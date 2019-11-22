bdeb9aa83fe7196a0446cfe4b3bf5a8d03a79be3
Dimitris Glaros <glaros.dimitrios@gmail.com>
2019-11-22 13:17:16 +0200

    Add createdAt and modifiedAt in PaymentType Entity

--- src/Entity/PaymentType.php
+++ src/Entity/PaymentType.php
@@ -21,6 +21,16 @@ class PaymentType
      */
     private $name;
 
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $modifiedAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +44,30 @@ class PaymentType
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeImmutable
+    {
+        return $this->modifiedAt;
+    }
+
+    public function setModifiedAt(\DateTimeImmutable $modifiedAt): self
+    {
+        $this->modifiedAt = $modifiedAt;
 
         return $this;
     }
