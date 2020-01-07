6e4581e40255ffbcd98aa3c535c5ec5ec18db702
Dimitris Glaros <glaros.dimitrios@gmail.com>
2020-01-08 00:19:02 +0200

    Make Payment Entity json serializable

--- src/Entity/Payment.php
+++ src/Entity/Payment.php
@@ -4,49 +4,57 @@ namespace App\Entity;
 
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use JsonSerializable;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PaymentRepository")
  * @ORM\HasLifecycleCallbacks
  */
-class Payment
+class Payment implements JsonSerializable
 {
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue(strategy="UUID")
      * @ORM\Column(type="guid")
+     * @var string
      */
     private $id;
 
     /**
      * @ORM\Column(type="datetime_immutable")
+     * @var DateTimeImmutable
      */
     private $createdAt;
 
     /**
      * @ORM\Column(type="datetime_immutable")
+     * @var DateTimeImmutable
      */
     private $modifiedAt;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="payments")
      * @ORM\JoinColumn(nullable=false)
+     * @var User
      */
     private $user;
 
     /**
      * @ORM\ManyToOne(targetEntity="App\Entity\PaymentType")
      * @ORM\JoinColumn(nullable=false)
+     * @var PaymentType
      */
     private $type;
 
     /**
      * @ORM\Column(type="integer")
+     * @var int
      */
     private $amount;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * @var string
      */
     private $description;
 
@@ -142,5 +150,18 @@ class Payment
     public function onPreUpdate()
     {
         $this->modifiedAt = new DateTimeImmutable();
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'id' => $this->id,
+            'userId' => $this->user->getId(),
+            'paymentTypeId' => $this->type->getId(),
+            'description' => $this->description,
+            'amount' => $this->amount,
+            'createdAt' => $this->createdAt->format('Y-m-d H:i:s'),
+            'modifiedAt' => $this->modifiedAt->format('Y-m-d H:i:s')
+        ];
     }
 }
